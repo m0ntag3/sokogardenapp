48271ea8d02cbe92bf6e5a18abae05fa2921da6c
m0ntag3 <kuminacollins@gmail.com>
2026-03-12 14:05:33 +0300

    sga3

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,19 +1,75 @@
+import axios from 'axios';
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
 const Signup = () => {
     // Initialize the hooks
     const [username,setUsername]=useState("");
+    // username= name of the hook \|/ setUsername= function
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
     const [number,setNumber] = useState("");
-    
+    // console.log(username)
+
+    // Define the three state of application will move to
+    const[loading,setLoading]=useState("");
+    const[success,setSuccess]=useState("");
+    const[error,setError]=useState("");
+
+    // Below is a function to handle the submit action
+    const handleSubmit = async(e) => {
+        // Below we prevent our site from reloading
+        e.preventDefault()
+
+        // Update our loading hook with a message that will be displayed to the users who will try to register
+        setLoading("Please bear with us while we finalize your registration. This will only take a moment.")
+        try{
+            // Create a form data object that will enable you to capture the four detils entered on the form
+            const formdata = new FormData();
+
+            // Insert the four details(username,email,password,phone number) interms of key-value pairs.
+            formdata.append("username", username);
+            formdata.append("email", email);
+            formdata.append("password", password);
+            formdata.append("phone", number);
+
+            // By use of Axois, we can access the method (POST)
+            const response = await axios.post("https://collinspaul.alwaysdata.net/api/signup", formdata);
+
+            // Set back the loading to default
+            setLoading("");
+
+            //  Just incase everything goes well update the success hook with a message.
+            setSuccess(response.data.message)
+
+            // Clear the hooks
+            setUsername("");
+            setEmail("");
+            setPassword("");
+            setNumber("");
+        }
+        catch(error){
+            // set the loading book back to default
+            setLoading("");
+
+            // Update the error hook with the message given back from the response
+            setError(error.message)
+        }
+
+    }
+
 return (
     <div className='row justify-content-center mt-4'>
-        <div className="card col-md-6 shadow p-4">
+        <div className="card col-md-6 shadow p-4 bg-dark">
             <h1 className='text-warning'>Sign Up</h1>
 
-            <form>
+            <h5 className='text-warning'>{loading}</h5>
+            <h3 className='text-success'>{success}</h3>
+            <h4 className="text-danger">{error}</h4>
+
+            <form onSubmit={handleSubmit}>
+
+
             <input type="text" 
             placeholder='Enter the Username'
             className='form-control' 
@@ -48,9 +104,9 @@ return (
             onChange={(e) => setNumber(e.target.value)}
             required/> <br />
 
-            {number} <br />
+            {/* {number} <br /> */}
 
-            <input type="button" value="Signup" className="btn btn-outline-danger" /> <br /> <br />
+            <input type="submit" value="Signup" className="btn btn-outline-danger" /> <br /> <br />
 
             <p className='last'>Already have an account?<Link to={'/signin'} className='link'>Sign In</Link></p>
             </form>
@@ -62,7 +118,7 @@ return (
 export default Signup;
 
 // Axios 
-    // In React, Axios is perfered to fetch() since Axios can hold many things at a time.
+    // In React, Axios is prefered to fetch() since Axios can hold many things at a time.
     // Axios is also considered automatic in comparison to fetch().
     // Axios is prefered because;
 
